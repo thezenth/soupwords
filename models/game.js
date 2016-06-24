913fb73350291641093cbdf13f0faf7ca9d19773
Noah Wolfe <Noah Wolfe>
2016-06-23 23:10:44 -0400

    Created model for game.json session data

---
+++ models/game.js
@@ -0,0 +1,15 @@
+var Game = {
+    "players": [], //array of Player objs
+    "letters": [] //multidimensional array, see below
+}
+
+/*
+* multidimensional array structure
+* [ [ X, X, X ],
+*   [ X, X, X ],
+*   [ X, X, X ] ]
+*/
+
+module.exports = {
+    Game: Game
+}
---
+++ models/player.js
@@ -0,0 +1,8 @@
+var Player = {
+    "words": [],
+    "points": 0
+}
+
+module.exports = {
+    Player: Player
+}
